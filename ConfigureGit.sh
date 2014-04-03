930a1b783976941f1d6d9bf2e8ab2011929a17c5
Anthony Bardessono <abardess@calpoly.edu>
2014-04-03 15:02:00 -0700

    track Config script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Anthony Bardessono"
+git config --global user.email "abardess@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
